72f24dbb0c5c4c63fe4fc8314e0f968771b7a7b2
Vasili Zlobin <vaszlbn@gmail.com>
2022-12-30 01:48:57 +0300

    initial commit

---
+++ src/test/java/ru/vasili_zlobin/web_market/WebMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasili_zlobin.web_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
